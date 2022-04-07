71f161aa81f86267de714f211e3dee139f692d68
Raghunathan Ramakrishnan <ramakrishnan@tifrh.res.in>
2022-04-07 07:00:58 +0530

    rr: minor edits

--- local_install.sh
+++
@@ -1,2 +0,0 @@
-# Kept for raghurama123's purpose. Feel free to adapt it if needed
-cp qmlspectrum/* /Users/raghurama/.local/lib/python3.6/site-packages/qmlspectrum/
--- push_to_pypi.sh
+++
@@ -1,5 +0,0 @@
-rm -rf qmlspectrum.egg-info
-rm -rf dist
-
-python3.6 setup.py sdist
-python3.6 /Users/raghurama/anaconda3/lib/python3.6/site-packages/twine/__main__.py upload dist/*
