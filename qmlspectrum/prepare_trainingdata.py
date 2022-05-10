0b3a459f5265371dcf2eeff3ee756f337a0aad68
Arpan Choudhury <32363671+arpanchoudhury@users.noreply.github.com>
2022-05-10 17:21:08 +0530

    ac: kernel for SLATM is added

--- qmlspectrum/prepare_trainingdata.py
+++ qmlspectrum/prepare_trainingdata.py
@@ -1,31 +1,56 @@
 import numpy as np
 import qml
+from qml.kernels import laplacian_kernel, gaussian_kernel
 
-def prepare_trainingdata(N_train,load_K,file_kernel,indices,lamd,X,Int_lam,sigmas,cut_distance,max_size):
+
+def prepare_trainingdata(descriptor,N_train,load_K,file_kernel,indices,lamd,X,Int_lam,sigmas,cut_distance,max_size):
 
 
     N_bin=len(Int_lam[0])
 
     K=np.zeros([N_train,N_train],dtype=float)
-    if load_K:
-        K = np.load(file_kernel)
-    else:
-        print('Calculating FCHL kernel elements'+'\n')
-        for itrain in range(N_train):
-            K[itrain,itrain]=1.0 + lamd
-            if np.mod(itrain,10) == 0:
-                print(itrain, 'rows calculated', N_train-itrain, 'remaining')
-            for jtrain in range(itrain+1,N_train):
-                Xt=X[indices[itrain]]
-                Xq=X[indices[jtrain]]
-                Yt=np.zeros([1,max_size,5,max_size],dtype=float)
-                Yq=np.zeros([1,max_size,5,max_size],dtype=float)
-                Yt[0]=Xt
-                Yq[0]=Xq
-                tmp=qml.fchl.get_global_kernels(Yq,Yt,sigmas=sigmas,cut_distance=cut_distance)
-                K[itrain,jtrain]=tmp[0,0,0]
-                K[jtrain,itrain]=K[itrain,jtrain]
-        np.save(file_kernel, K)
+
+    if descriptor == 'FCHL':
+        if load_K:
+            K = np.load(file_kernel)
+        else:
+            print('Calculating FCHL kernel elements'+'\n')
+            for itrain in range(N_train):
+                K[itrain,itrain]=1.0 + lamd
+                if np.mod(itrain,10) == 0:
+                    print(itrain, 'rows calculated', N_train-itrain, 'remaining')
+                for jtrain in range(itrain+1,N_train):
+                    Xt=X[indices[itrain]]
+                    Xq=X[indices[jtrain]]
+                    Yt=np.zeros([1,max_size,5,max_size],dtype=float)
+                    Yq=np.zeros([1,max_size,5,max_size],dtype=float)
+                    Yt[0]=Xt
+                    Yq[0]=Xq
+                    tmp=qml.fchl.get_global_kernels(Yq,Yt,sigmas=sigmas,cut_distance=cut_distance)
+                    K[itrain,jtrain]=tmp[0,0,0]
+                    K[jtrain,itrain]=K[itrain,jtrain]
+            np.save(file_kernel, K)
+
+    if descriptor == 'SLATM':
+        if load_K:
+            K = np.load(file_kernel)
+        else:
+            print('Calculating SLATM kernel elements'+'\n')
+            for itrain in range(N_train):
+                K[itrain,itrain]=1.0 + lamd
+                if np.mod(itrain,10) == 0:
+                    print(itrain, 'rows calculated', N_train-itrain, 'remaining')
+                for jtrain in range(itrain+1,N_train):
+                    Xt=X[indices[itrain]]
+                    Xq=X[indices[jtrain]]
+                    Yt=np.zeros([1,len(Xt)],dtype=float)
+                    Yq=np.zeros([1,len(Xq)],dtype=float)
+                    Yt[0]=Xt
+                    Yq[0]=Xq
+                    tmp=laplacian_kernel(Yq,Yt, sigma=100)
+                    K[itrain,jtrain]=tmp[0,0]
+                    K[jtrain,itrain]=K[itrain,jtrain]
+            np.save(file_kernel, K)
 
     P=np.zeros([N_train,N_bin],dtype=float)
 
@@ -34,6 +59,7 @@ def prepare_trainingdata(N_train,load_K,file_kernel,indices,lamd,X,Int_lam,sigma
             P[itrain,ibin]=Int_lam[indices[itrain],ibin]
 
     return K, P
+
 
 
 def prepare_trainingdata_batch(N_train,file_kernel,indices,lamd,X,Int_lam,sigmas,cut_distance,max_size, s_batch, i_batch):
